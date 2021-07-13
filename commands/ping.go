f46247e38f6ce52d2e86ab8eae61494a5681f788
Galen Guyer <galen@galenguyer.com>
2021-07-13 15:03:56 -0700

    add time for ping command

--- commands/ping.go
+++ commands/ping.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"fmt"
+
 	"github.com/bwmarrin/discordgo"
 	"github.com/galenguyer/genericbot/entities"
 )
@@ -8,12 +10,30 @@ import (
 var Ping = &entities.Command{
 	Name: "ping",
 	Action: func(c entities.Context) error {
-		_, err := c.Session.ChannelMessageSendComplex(c.Message.ChannelID, &discordgo.MessageSend{
+		msg, err := c.Session.ChannelMessageSendComplex(c.Message.ChannelID, &discordgo.MessageSend{
 			Content:         "pong!",
 			TTS:             false,
 			AllowedMentions: &discordgo.MessageAllowedMentions{},
 			Reference:       c.Message.Reference(),
 		})
+		if err != nil {
+			return err
+		}
+
+		cmdCreation, _ := discordgo.SnowflakeTimestamp(c.Message.ID)
+		rplCreation, _ := discordgo.SnowflakeTimestamp(msg.ID)
+		timeDiff := rplCreation.Sub(cmdCreation)
+		diffString := fmt.Sprintf("pong! time taken: `%dms`", timeDiff.Milliseconds())
+
+		_, err = c.Session.ChannelMessageEditComplex(
+			&discordgo.MessageEdit{
+				Content:         &diffString,
+				AllowedMentions: &discordgo.MessageAllowedMentions{},
+
+				ID:      msg.ID,
+				Channel: msg.ChannelID,
+			},
+		)
 		return err
 	},
 }

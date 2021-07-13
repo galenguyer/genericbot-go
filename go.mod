8d543e7b936d193835926825d36b300718a7823a
Galen Guyer <galen@galenguyer.com>
2021-07-13 07:56:05 -0700

    add connection to discord through sharding

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/galenguyer/genericbot
 go 1.16
 
 require (
+	github.com/bwmarrin/discordgo v0.23.3-0.20210410202908-577e7dd4f6cc
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
+	github.com/servusdei2018/shards v1.2.1
 	github.com/sirupsen/logrus v1.8.1
 	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
 	"github.com/galenguyer/genericbot/config"
 	"github.com/galenguyer/genericbot/logging"
+	"github.com/servusdei2018/shards"
 	"github.com/sirupsen/logrus"
 )
 
@@ -17,4 +23,29 @@ func main() {
 	logging.Logger.WithFields(logrus.Fields{"module": "genericbot", "method": "main"}).Info("starting genericbot")
 	logging.Logger.WithFields(logrus.Fields{"module": "genericbot", "method": "main"}).Info("using prefix " + config.BotConfig.Prefix)
 	logging.Logger.WithFields(logrus.Fields{"module": "genericbot", "method": "main"}).Info("using log level " + config.BotConfig.LogLevel.String())
+
+	manager, err := shards.New("Bot " + config.BotConfig.Token)
+	if err != nil {
+		logging.Logger.WithFields(logrus.Fields{"error": err, "module": "genericbot", "method": "main"}).Error("error creating shard manager")
+	}
+
+	manager.AddHandler(onShardConnect)
+
+	err = manager.Start()
+	if err != nil {
+		logging.Logger.WithFields(logrus.Fields{"error": err, "module": "genericbot", "method": "main"}).Error("error starting shard manager")
+	}
+
+	// wait for ^C to exit
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	logging.Logger.WithFields(logrus.Fields{"module": "genericbot", "method": "main"}).Infof("stopping shard manager with %d shards", manager.ShardCount)
+	manager.Shutdown()
+	logging.Logger.WithFields(logrus.Fields{"module": "genericbot", "method": "main"}).Info("stopped shard manager")
+}
+
+func onShardConnect(s *discordgo.Session, evt *discordgo.Connect) {
+	logging.Logger.WithFields(logrus.Fields{"module": "genericbot", "method": "onShardConnect", "shard": s.ShardID}).Info("shard connected")
 }

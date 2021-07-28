f4b5bae2521cf7e361fdd307f94758c8a0f8f5eb
Galen Guyer <galen@galenguyer.com>
2021-07-27 22:07:33 -0700

    mock reply feature

--- commands/mock.go
+++ commands/mock.go
@@ -5,7 +5,10 @@ import (
 	"math/rand"
 	"unicode"
 
+	"github.com/bwmarrin/discordgo"
 	"github.com/galenguyer/genericbot/entities"
+	"github.com/galenguyer/genericbot/logging"
+	"github.com/sirupsen/logrus"
 )
 
 var Mock = &entities.Command{
@@ -13,8 +16,37 @@ var Mock = &entities.Command{
 	Description: "Mock someone ruthlessly",
 	Usage:       "<text>",
 	Action: func(c entities.Context) error {
-		_, err := c.Reply(mockText(c.ParsedCommand.ParameterString))
-		return err
+		if c.Message.MessageReference != nil {
+			message, err := c.Session.ChannelMessage(c.ChannelId, c.Message.MessageReference.MessageID)
+			if err != nil {
+				c.Reply("Sorry, I couldn't find the message you were replying to")
+				return err
+			}
+
+			// delete the original message and log failures
+			err = c.Session.ChannelMessageDelete(c.ChannelId, c.Message.ID)
+			if err != nil {
+				logging.Logger.WithFields(logrus.Fields{
+					"error":    err,
+					"guild":    c.GuildId,
+					"channel":  c.ChannelId,
+					"author":   c.Message.Author.ID,
+					"messsage": c.Message.ID,
+					"command":  "ping",
+				}).Error("could not delete message")
+			}
+
+			_, err = c.Session.ChannelMessageSendComplex(c.ChannelId, &discordgo.MessageSend{
+				Content:         mockText(message.Content),
+				TTS:             false,
+				AllowedMentions: &discordgo.MessageAllowedMentions{},
+				Reference:       message.Reference(),
+			})
+			return err
+		} else {
+			_, err := c.Reply(mockText(c.ParsedCommand.ParameterString))
+			return err
+		}
 	},
 }
 

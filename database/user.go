b9817b499f9f5661fb41aa4aaa0d6985e0b5aa31
Galen Guyer <galen@galenguyer.com>
2021-07-30 21:32:38 -0700

    add method for getting user by id from database

--- database/user.go
+++ database/user.go
@@ -11,6 +11,29 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+func GetUser(guildId string, userId string) (*entities.User, error) {
+	logging.Logger.WithFields(logrus.Fields{"module": "database", "method": "GetUser", "guild": guildId, "user": userId}).Info("getting user")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	var user entities.User
+	err := Client.Database(guildId).Collection("users").FindOne(ctx, bson.D{{Key: "_id", Value: userId}}).Decode(&user)
+	if err == mongo.ErrNoDocuments {
+		return nil, nil
+	} else if err != nil {
+		logging.Logger.WithFields(logrus.Fields{
+			"error":  err,
+			"module": "database",
+			"method": "GetUser",
+			"guild":  guildId,
+			"user":   userId,
+		}).Error("error searching for user")
+		return nil, err
+	}
+	return &user, nil
+}
+
 func SaveUser(guildId string, user entities.User) (UpsertResult, error) {
 	logging.Logger.WithFields(logrus.Fields{"module": "database", "method": "SaveUser", "guild": guildId, "user": user.Id}).Info("saving user")
 
